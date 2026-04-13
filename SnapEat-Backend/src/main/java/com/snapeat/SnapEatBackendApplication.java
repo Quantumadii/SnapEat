023e3fdf5861590437b5f37bc5021401546d1930
Pawan Gote <pawangote77@gmail.com>
2026-04-13 18:10:23 +0530

    Restore missing backend source and email template files

---
+++ SnapEat-Backend/src/main/java/com/snapeat/SnapEatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.snapeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapEatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapEatBackendApplication.class, args);
+	}
+
+}
